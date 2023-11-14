8fb5285d8171f2d732e9820d4e1dc05ae3e4fcd5
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2023-11-14 12:02:29 +0100

    finished first step for counter

--- lib/screen/second_screen.dart
+++ lib/screen/second_screen.dart
@@ -1,57 +1,49 @@
+import 'dart:async';
+import 'dart:ffi';
+
+import 'package:count_down/count_downtimer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_countdown_timer/countdown_timer_controller.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:timer_count_down/timer_count_down.dart';
+
 class SecondScreen extends StatefulWidget {
   @override
   _SecondScreenState createState() => _SecondScreenState();
 }
 
 class _SecondScreenState extends State<SecondScreen> {
-  List<dynamic> name = [
-    'Alice',
-    'Bob',
-    'Charlie',
-    'David',
-    'Emma',
-    'Fred',
-    'Grace',
-    'Henry',
-    'Ivy',
-    'Jack'
-        'Alice',
-    'Bob',
-    'Charlie',
-    'David',
-    'Emma',
-    'Fred',
-    'Grace',
-    'Henry',
-    'Ivy',
-    'Jack'
-        'Alice',
-    'Bob',
-    'Charlie',
-    'David',
-    'Emma',
-    'Fred',
-    'Grace',
-    'Henry',
-    'Ivy',
-    'Jack'
-  ];
+  Duration duration = Duration();
+  Timer? timer;
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second Screen'),
-      ),
-      body: ListView.builder(
-          itemCount: name.length,
-          itemBuilder: (BuildContext context, index) {
-            final names = name[index];
-            return ListTile(
-              title: Text(names),
-            );
-          }),
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  void addTime() {
+    final addSeconds = 1;
+    setState(() {
+      final seconds = duration.inSeconds + addSeconds;
+      duration = Duration(seconds: seconds);
+    });
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (_) => addTime());
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
+          child: BuildTime(),
+        ),
+      );
+
+  Widget BuildTime() {
+    return Text(
+      '${duration.inSeconds}',
+      style: TextStyle(fontSize: 80),
     );
   }
 }
